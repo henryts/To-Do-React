ad6b2e00b170851745aad65dad513dbc4e592414
henry sathyan <henrysathyan@gmail.com>
2023-05-17 19:35:29 +0530

    todo app- initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+
+import { useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+  const [toDos, setToDos] = useState([]);
+const [toDo, setToDo] = useState(' ');
   return (
     <div className="app">
       <div className="mainHeading">
@@ -12,20 +18,42 @@ function App() {
         <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
       </div>
       <div className="input">
-        <input type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
+        <input type="text"  onChange={(e)=>setToDo(e.target.value)} placeholder="🖊️ Add item..." />
+        <i onClick={()=>{setToDos([...toDos,{id:Date.now(), text:toDo,status:false}])}} className="fas fa-plus"></i>
       </div>
       <div className="todos">
-        <div className="todo">
+        {toDos.map((value)=>{
+      return (  <div className="todo">
           <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>Rect tutorial</p>
+            <input onChange={(e)=>{console.log(e.target.checked);
+              console.log(value);
+              setToDo(toDos.filter((obj)=>{
+                if(value.id===obj.id)
+                {
+                  value.status=e.target.checked;
+                }
+                return obj;
+              }))
+              
+              
+              }} value={value.status} type="checkbox" name="" id="" />
+            <p>{value.text}</p>
           </div>
           <div className="right">
             <i className="fas fa-times"></i>
           </div>
         </div>
+      );})}
+       { toDos.map((obj)=>{
+      if(obj.status)
+      {
+        return (<h1>{obj.text}</h1>)
+      }
+      return null;
+    })
+    }
       </div>
+    
     </div>
   );
 }
